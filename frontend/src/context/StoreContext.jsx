16ebcefa20fa61bfac871fec160526a3baf9b654
ozibryan <ozibryan@yahoo.com>
2024-09-08 11:49:34 +1000

    Update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -52,6 +52,8 @@ const StoreContextProvider = (props) => {
     const fetchFoodList = async () => {
         const response = await axios.get(url + "/api/food/list");
         setFoodList(response.data.data)
+
+        console.log(food_list)
     }
 
     const loadCartData = async (token) => {
